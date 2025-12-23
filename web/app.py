975c6e846a89cc5f6b3e255280578db5b7110a47
Maria <mariazcona2017@gmail.com>
2025-12-23 16:57:10 +0100

    Fix Image Render

--- web/app.py
+++ web/app.py
@@ -22,36 +22,40 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🍺 CapCollection")
-st.caption("Web version of your bottle cap collection")
+st.title("Maria's Collection")
+st.caption("La colección de chapas de botella de Maria A.G.")
 st.markdown("<style> .stImage img {border-radius: 8px;} </style>", unsafe_allow_html=True)
 
 # --------------------------------------------------
 # Buscar por marca
 # --------------------------------------------------
-st.subheader("Search by brand")
+st.subheader("Búsqueda por Marca")
 
 marca = st.text_input(
-    "Brand name",
-    placeholder="e.g. Coca-Cola, Heineken..."
+    "Marca",
+    placeholder="p. ej. Coca-Cola, Heineken..."
 )
 
 if marca:
     resultados = fn.buscar_por_marca(marca)
 
     if not resultados:
-        st.info("No results found.")
+        st.info("No se han encontrado resultados.")
     else:
-        st.success(f"{len(resultados)} result(s) found")
+        st.success(f"{len(resultados)} resultados encontrados")
         for r in resultados:
             col1, col2 = st.columns([1, 3])
+            print(r[0])
+            print(r[1])
+            print(r[2])
+            print(r[3])
             
             # Imagen
-            img_file = BASE_DIR / "images" / r[3]
+            img_file = BASE_DIR/r[3]
             if img_file.exists():
                 col1.image(str(img_file), width=100)
             else:
-                col1.text("No image")
+                col1.text("No hay imagen.")
             
             # Info
             col2.markdown(f"**Marca:** {r[1]}")  # índice 1 = marca
